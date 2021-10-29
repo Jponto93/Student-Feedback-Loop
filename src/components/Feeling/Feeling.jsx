c8282772b8258b3c725ca672de70d0ae6f31584f
Jordan Ponto <jponto93@gmail.com>
2021-10-29 16:30:06 -0500

    Feeling component set up, need to make button functional

---
+++ src/components/Feeling/Feeling.jsx
@@ -0,0 +1,14 @@
+
+
+function Feeling() {
+    return (
+        <>
+            <h2>How are you feeling today?</h2>
+            <input type="number" 
+            placeholder="Feeling?"/>
+            <button>NEXT</button>
+        </>
+    )
+} // end Feeling
+
+export default Feeling;
