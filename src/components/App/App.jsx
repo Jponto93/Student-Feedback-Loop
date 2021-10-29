059183ff721874962e07faafff76e433a75138ae
Jordan Ponto <jponto93@gmail.com>
2021-10-29 16:52:53 -0500

    moved header into component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,16 +5,13 @@ import Feeling from '../Feeling/Feeling';
 import Understanding from '../Understanding/Understanding';
 import Support from '../Support/Support';
 import Comments from '../Comments/Comments';
+import Header from '../Header/Header';
 
 function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-
+      <Header />
       <Feeling />
       <Understanding />
       <Support />
