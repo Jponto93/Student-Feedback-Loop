ad44ecd814818415807b0f47ff9efd656cdf87e9
Jordan Ponto <jponto93@gmail.com>
2021-10-29 17:00:05 -0500

    redux stuff imported, began reducers, 5 o clock lets get outta here

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+// set up redux
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+
+// REDUCERS
+//reducer to hold/modify contents of feedback
+const feedback = (state = template, action) => {
+    
+}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
