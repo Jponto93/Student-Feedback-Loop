cfc9b45c0426df30b0411cc6f473aff4bf737542
Jordan Ponto <jponto93@gmail.com>
2021-10-31 13:34:38 -0500

    table set up

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -2,7 +2,20 @@
 
 function Admin () {
     return (
-        <p>Test</p>
+        <table>
+            <thead>
+                <tr>
+                    <th>Feeling</th>
+                    <th>Comprehension</th>
+                    <th>Support</th>
+                    <th>Comments</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
     )
 } // end Admin
 
